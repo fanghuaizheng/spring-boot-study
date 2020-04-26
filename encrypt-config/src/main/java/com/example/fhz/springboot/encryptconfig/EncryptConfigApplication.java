0b8a5fff0734e20b486eacefd0896fd49eecaf0a
fanghuaizheng <fang101056@outlook.com>
2020-04-26 11:31:40 +0800

    Initial commit

---
+++ encrypt-config/src/main/java/com/example/fhz/springboot/encryptconfig/EncryptConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.fhz.springboot.encryptconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EncryptConfigApplication.class, args);
+    }
+
+}
---
+++ encrypt-config/src/test/java/com/example/fhz/springboot/encryptconfig/EncryptConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fhz.springboot.encryptconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
