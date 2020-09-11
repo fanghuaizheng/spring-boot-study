45524599cbe0d5742c5e22258050412c61a96271
huaizheng.fang@jvc-vwg.com <huaizheng.fang@jvc-vwg.com>
2020-09-11 16:19:39 +0800

    加入接口幂等性校验

--- fhz-server-a/src/main/java/com/example/fhz/a/controller/TestController.java
+++ fhz-server-a/src/main/java/com/example/fhz/a/controller/TestController.java
@@ -1,10 +1,15 @@
 package com.example.fhz.a.controller;
 
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.fhz.a.annotation.Idempotent;
 import com.example.fhz.a.feign.FeignOne;
 import com.example.fhz.a.service.TestService;
 
@@ -14,11 +19,34 @@ import lombok.extern.slf4j.Slf4j;
 public class TestController {
 	
 	@Autowired
+	StringRedisTemplate redisTemplate;
+	
+	private final String token_pref="token:";
+	
+	@Autowired
 	FeignOne feignOne;
 
 	@Autowired
     private TestService service;
+	
+	@GetMapping("getToken")
+	public String getToken() {
+		String token = UUID.randomUUID().toString().replace("-", "");
+		String key = token_pref+token;
+		redisTemplate.opsForValue().set(key, "1");
+		
+		redisTemplate.expire(key, 30, TimeUnit.MINUTES);
+		return token;
+	}
 
+	@GetMapping("checkToken")
+	public Object gcheckToken(String token ) {
+		String key = token_pref+token;
+		return redisTemplate.opsForValue().get(key);
+		
+	}
+	
+	@Idempotent
     @GetMapping(value = "/hello/{name}")
     public String apiHello(@PathVariable String name) {
         return service.sayHello(name);
